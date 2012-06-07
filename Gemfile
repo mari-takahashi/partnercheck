3eea4ddba84cf41967e17813f47a9b292f92745c
Mari Takahashi <kisth77@gmail.com>
2012-06-07 21:39:08 +0900

    友達とってくる、ランダムに友達を表示、Facebookへシェア

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem 'sinatra'
 gem 'active_support'
 gem 'omniauth'
 gem 'omniauth-facebook'
+gem 'fb_graph'
 
 group :development do
   gem 'heroku'
--- app.rb
+++ app.rb
@@ -1,34 +1,62 @@
-# myapp.rb
+# encoding:utf-8
 require 'sinatra'
 require 'erb'
 require 'omniauth'
 require 'omniauth-facebook'
+require 'fb_graph'
 
 enable :sessions
 
 use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], :scope => "email"
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], :scope => "email,publish_stream"
 end
+
 
 get '/auth/facebook/callback' do
   @auth = request.env['omniauth.auth']
-  session[:name] = @auth[:info][:name]
-  erb :callback
-end
-
-
-post '/res' do
-  test = params[:hoge1].to_i + params[:hoge2].to_i
-  
-  if test > 10
-    @a = "over 10"
+  access_token = @auth[:credentials][:token]
+  user = FbGraph::User.me(access_token)
+  user = user.fetch
+  friends = user.friends
+  @partner = friends[rand(friends.length)]
+  @partner = @partner.fetch
+  session[:token] = access_token
+  if @partner.gender == user.gender
+    @partner = nil
+    session[:partner_name] = nil
   else
-    @a = "small 10"
+    session[:partner_name] = @partner.name
+    session[:partner_link] = @partner.link
   end
-  
-  erb :result
+  erb :callback
 end
 
 get '/' do
   erb :index
+end
+
+post '/share' do
+  me = FbGraph::User.me(session[:token])
+  begin
+    if session[:partner_name]
+      me.feed!(
+      :message => '私の来世のパートナーはこの人でした！',
+      :link => session[:partner_link],
+      #:link => 'https://github.com/nov/fb_graph',
+      :name => session[:partner_name],
+      :description => session[:partner_name]
+      )
+    else
+       me.feed!(
+       :message => '私は来世でパートナーがいないみたいです。',
+       )
+    end
+  rescue => e
+    puts e.message
+  end
+  redirect '/result'
+end
+
+get '/result' do
+  erb :result
 end
